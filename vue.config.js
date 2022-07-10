31a2f938ed553f7c65cf5ee9c6ea6c38a5d927bb
kwesi Bazil <kwesibazil@gmail.com>
2022-07-10 16:54:32 -0400

    .

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,7 @@ module.exports = defineConfig({
       .rule('images')
         .set('parser', {
           dataUrlCondition: {
-            maxSize: 4 * 1024 // 4KiB
+            maxSize: 1 * 1024 // 4KiB
           }
         })
   }
